f20a0188ed5d301cdd0f614d58f1cb743851de2a
Tasawar Sattar <tasawar.sattar@mail.utoronto.ca>
2017-02-28 23:59:42 -0500

    Created Snippet

---
+++ README.md
@@ -0,0 +1,16 @@
+Snippet
+
+// Created by Refinery Inc
+// Contributors: Hairuo Sang
+//             : Tasawar Sattar
+
+
+
+Unfolded the truth on how to read on devices. The real way to read.
+
+Paperbacks are meant to be cluttered and full. Supposed to drown in the glory of ink on the lands of paper.
+Our devices are different. We prefer to have our screens be minimally intrusive and deliver our us ioration
+in small portions.
+
+Read your ebooks in 'snips': break up the material to be read and have the message be presented/conveyed
+in small snippets rather than the full thing at once.
---
+++ Snippet/CollectionViewController.swift
@@ -0,0 +1,121 @@
+//
+//  CollectionViewController.swift
+//  Snippet
+//
+//  Created by Paul on 2017-02-28.
+//  Copyright © 2017 Refinery. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "BookCell"
+
+class CollectionViewController: UICollectionViewController, UIPopoverPresentationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // make the edges of the optionsView rounded
+//        optionsView.layer.cornerRadius = 4
+        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func OptionsPopover(_ sender: Any) {
+        self.performSegue(withIdentifier: "OptionsSegue", sender: self)
+        
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "OptionsSegue"{
+            var vc = segue.destination
+            
+            var controller = vc.popoverPresentationController
+            
+            if controller != nil{
+                controller?.delegate = self
+            }
+        }
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
+    
+    
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 16
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        // dummy background color
+        cell.backgroundColor = UIColor.black
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+}
